762b9443cc9486d13da2d564598bc912578bbe79
Gerald Versluis <gerald@verslu.is>
2018-12-13 10:52:11 +0100

    Initial commit

---
+++ WebViewNavigatingSample/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace WebViewNavigatingSample
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		public void Handle_Navigating(object sender, WebNavigatingEventArgs e)
+		{
+			if (e.Url.StartsWith("https://stackoverflow.com", StringComparison.Ordinal))
+				return;
+
+			Device.OpenUri(new Uri(e.Url));
+
+			e.Cancel = true;
+		}
+	}
+}
